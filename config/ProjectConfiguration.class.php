6f03c3c3a813f3adbd445fa423f463fea74c919a
Marc Montañés <marc@teclliure.net>
2011-03-21 18:45:21 +0100

    * DB design (using orm designer)

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '/home/marc/workspace/symfony-1.4/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+    $this->enablePlugins('sfDoctrineGuardPlugin');
+  }
+}
