0c21ee41142fe638447b38947920b9198824bfb0
Marc Montañés <marc@teclliure.net>
2011-05-03 18:45:35 +0200

    * Styled some more pages

--- apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
+++ apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -1,7 +1,29 @@
 <?php use_helper('I18N') ?>
 
-<div id="login_box" class="ui-widget-content ui-corner-all">
-  <h1><?php echo __('Signin', null, 'sf_guard') ?></h1>
-  
-  <?php echo get_partial('sfGuardAuth/signin_form', array('form' => $form)) ?>
+<div class="box no-tabs no-margin">
+  <div class="box-content show">
+
+    <div class="box-title">
+      <p><?php echo __('Signin', null, 'sf_guard') ?></p>
+    </div>
+    
+    <form class="form" action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
+      <?php echo $form->renderGlobalErrors() ?>
+      <?php echo $form->renderHiddenFields() ?>
+      <?php echo $form['username']->renderRow() ?>
+      <?php echo $form['password']->renderRow() ?>
+      <?php $routes = $sf_context->getRouting()->getRoutes() ?>
+      <?php if (isset($routes['sf_guard_forgot_password'])): ?>
+      <div>
+        <a href="<?php echo url_for('@sf_guard_forgot_password') ?>" class="remember"><?php echo __('Forgot your password?', null, 'sf_guard') ?></a>
+      </div>
+      <?php endif; ?>
+      <div>
+        <?php echo $form['remember']->render() ?> <?php echo __('Remember me') ?>
+      </div>
+      <br />
+      <button type="submit" class="button1"><?php echo __('Enter') ?> ></a>
+    </form>
+  </div>
+  <div class="clear"></div>
 </div>
--- lib/form/ProfileForm.class.php
+++ lib/form/ProfileForm.class.php
@@ -5,8 +5,16 @@ class ProfileForm extends sfGuardRegisterForm {
   {
     unset($this['email'],$this['password'],$this['password_again'],$this['email_address'],$this['actions_list'],$this['opinions_list']);
     
+    $decorator = new sfWidgetFormSchemaFormatterFrontend($this->widgetSchema, $this->validatorSchema);
+    $this->widgetSchema->addFormFormatter('custom', $decorator);
+    $this->widgetSchema->setFormFormatterName('custom');
+    
     $profileForm = new sfGuardUserProfileForm($this->getObject()->getProfile());
     unset($profileForm['user_id'],$profileForm['subscription_email'],$profileForm['categories_list'],$profileForm['regions_list']);
+    $profileForm->widgetSchema->addFormFormatter('custom', $decorator);
+    $profileForm->widgetSchema->setFormFormatterName('custom');
     $this->embedForm('profile', $profileForm);
+    
+    
   }
 }
