9f9837a70a5f144daf7ee55406ddaa0a23a9cbd3
Juan Pablo Lozano <jplozano6@gmail.com>
2011-05-05 00:13:11 +0200

    * Signup up form template

---
+++ apps/frontend/modules/user/templates/registerForm.php
@@ -0,0 +1,134 @@
+
+    <form method="post" action="#" class="form" id="signup-form">
+
+	    <div class="bar-left">
+	    
+	    	<div class="box no-tabs no-margin">
+	    		<div class="box-content show">
+	    
+	    			<div class="box-title color1">
+	    				<p>Información de registre</p>
+	    			</div>
+	                
+	                <label>	
+	                	<span>Nom</span>
+	                	<input type="text" name="name" class="wide" />
+	                </label>
+	                
+	                <label>	
+	                	<span>Cognom</span>
+	                	<input type="text" name="lastname" class="wide" />
+	                </label>
+	                
+	                <label>	
+	                	<span>El teu Barri</span>
+	                	<select name="neighborhood">
+	                		<option value="">Seleccione</option>
+	                	</select>
+	                </label>
+	                
+	                <div class="clear"></div>
+	                <div class="line"></div>
+	                
+	                <label>	
+	                	<span>Nom d'Usuari</span>
+	                	<input type="text" name="username" class="wide" />
+	                </label>
+	                
+	                <label>	
+	                	<span>Data de naixement</span>
+	                	<input type="text" name="dob" class="wide calendar" />
+	                </label>
+	                
+	                <label>	
+	                	<span>Home</span>
+	                	<input type="checkbox" name="gender" value="m" />
+	                </label>
+	                
+	                <label>	
+	                	<span>Dona</span>
+	                	<input type="checkbox" name="gender" value="f" />
+	                </label>
+	                
+	            	<div class="clear"></div>
+	            	
+	            	<label>	
+	                	<span>Algo sobre ti</span>
+	                	<textarea name="description" class="wide"></textarea>
+	                </label>
+	                
+	                <div class="clear"></div>
+	                
+	                <label>	
+	                	<span>Nº Teléfon</span>
+	                	<input type="text" name="phone" class="wide" />
+	                </label>
+	                
+	                <label>	
+	                	<span>Website</span>
+	                	<input type="text" name="web" class="wide" />
+	                </label>
+	                
+	                <div class="clear"></div>
+	                
+	                <label>	
+	                	<span>Direcció</span>
+	                	<input type="text" name="address" class="wide address" />
+	                </label>
+	                
+	                <label>	
+	                	<span>C.P.</span>
+	                	<input type="text" name="zip" class="wide" />
+	                </label>
+					
+					<div class="clear"></div>
+					
+					<label>	
+	                	<span>Localidad</span>
+	                	<input type="text" name="city" class="wide" />
+	                </label>
+	                
+	                <label>	
+	                	<span>Provincia</span>
+	                	<input type="text" name="state" class="wide" />
+	                </label>
+	                
+					<label>	
+	                	<span>Pais</span>
+	                	<select name="county">
+	                		<option value="">Seleccione</option>
+	                	</select>
+	                </label>
+	                
+	                <div class="clear"></div>
+	
+	            </div>    
+	        </div>
+	    
+	    </div>
+	    
+	    <div class="bar-right">
+	    	<div class="box no-tabs no-margin">
+	    		<div class="box-content show">
+	    			
+	    			<div class="box-title color1">
+	    				<p>Inclou una foto</p>
+	    			</div>
+	    			
+	    			<label>
+	    				<span>Imatge:</span>
+	    				<input type="file" name="file" />
+	    				<div class="img profile"></div>
+	    			</label>
+	
+					<button class="button1">Hecho</button>
+	    			
+	    		</div>
+	    	</div>
+	    </div>
+	    
+	    <div class="clear"></div>
+    
+    </form> 
+    
+ 
