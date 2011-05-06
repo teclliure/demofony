1463c25e52e3c30704e035c915b6be4309c0cbba
Juan Pablo Lozano <jplozano6@gmail.com>
2011-05-06 12:06:07 +0200

    * changes on structure

--- apps/frontend/modules/user/templates/registerForm.php
+++ apps/frontend/modules/user/templates/registerForm.php
@@ -1,5 +1,5 @@
 
-    <form method="post" action="#" class="form" id="signup-form">
+    <form method="post" action="#" class="form" id="profile-profile">
 
 	    <div class="bar-left">
 	    
@@ -7,100 +7,137 @@
 	    		<div class="box-content show">
 	    
 	    			<div class="box-title color1">
-	    				<p>Información de registre</p>
+	    				<p><a href="#" class="toggler show">+</a>Información de registre</p>
 	    			</div>
+	    			
+	    			<div class="hide form-toggle-content">
 	                
-	                <label>	
-	                	<span>Nom</span>
-	                	<input type="text" name="name" class="wide" />
-	                </label>
+		                <label>	
+		                	<span>Nom</span>
+		                	<input type="text" name="name" class="wide" />
+		                </label>
+		                
+		                <label>	
+		                	<span>Cognom</span>
+		                	<input type="text" name="lastname" class="wide" />
+		                </label>
+		                
+		                <label>	
+		                	<span>El teu Barri</span>
+		                	<select name="neighborhood">
+		                		<option value="">Seleccione</option>
+		                	</select>
+		                </label>
+		                
+		                <div class="clear"></div>
+		                <div class="line"></div>
+		                
+		                <label>	
+		                	<span>Data de naixement</span>
+		                	<input type="text" name="dob" class="wide calendar" />
+		                </label>
+		                
+		                <label>	
+		                	<span>Home</span>
+		                	<input type="radio" name="gender" value="m" />
+		                </label>
+		                
+		                <label>	
+		                	<span>Dona</span>
+		                	<input type="radio" name="gender" value="f" />
+		                </label>
+		                
+		            	<div class="clear"></div>
+		            	
+		            	<label>	
+		                	<span>Algo sobre ti</span>
+		                	<textarea name="description" class="wide"></textarea>
+		                </label>
+		                
+		                <div class="clear"></div>
+		                
+		                <label>	
+		                	<span>Nº Teléfon</span>
+		                	<input type="text" name="phone" class="wide" />
+		                </label>
+		                
+		                <label>	
+		                	<span>Website</span>
+		                	<input type="text" name="web" class="wide" />
+		                </label>
+		                
+		                <div class="clear"></div>
+		                
+		                <label>	
+		                	<span>Direcció</span>
+		                	<input type="text" name="address" class="wide address" />
+		                </label>
+		                
+		                <label>	
+		                	<span>C.P.</span>
+		                	<input type="text" name="zip" class="wide" />
+		                </label>
+						
+						<div class="clear"></div>
+						
+						<label>	
+		                	<span>Localidad</span>
+		                	<input type="text" name="city" class="wide" />
+		                </label>
+		                
+		                <label>	
+		                	<span>Provincia</span>
+		                	<input type="text" name="state" class="wide" />
+		                </label>
+		                
+						<label>	
+		                	<span>Pais</span>
+		                	<select name="county">
+		                		<option value="">Seleccione</option>
+		                	</select>
+		                </label>
+		                
+		                <div class="clear"></div>
+		              
+		            </div>
 	                
-	                <label>	
-	                	<span>Cognom</span>
-	                	<input type="text" name="lastname" class="wide" />
-	                </label>
+	                <div class="box-title color1">
+	    				<p><a href="#" class="toggler">+</a>Cambiar contraseña</p>
+	    			</div>
+	    			
+	    			<div class="hide form-toggle-content">
+	    			
+		    			<label>	
+		                	<span>Contraseña actual</span>
+		                	<input type="password" name="current_password" />
+		                </label>
+		                
+		                <label>	
+		                	<span>Nueva contraseña</span>
+		                	<input type="password" name="current_password" />
+		                </label>
+		                
+		                <div class="clear"></div>
+		                
+		            </div>
 	                
-	                <label>	
-	                	<span>El teu Barri</span>
-	                	<select name="neighborhood">
-	                		<option value="">Seleccione</option>
-	                	</select>
-	                </label>
-	                
-	                <div class="clear"></div>
-	                <div class="line"></div>
-	                
-	                <label>	
-	                	<span>Nom d'Usuari</span>
-	                	<input type="text" name="username" class="wide" />
-	                </label>
-	                
-	                <label>	
-	                	<span>Data de naixement</span>
-	                	<input type="text" name="dob" class="wide calendar" />
-	                </label>
-	                
-	                <label>	
-	                	<span>Home</span>
-	                	<input type="checkbox" name="gender" value="m" />
-	                </label>
-	                
-	                <label>	
-	                	<span>Dona</span>
-	                	<input type="checkbox" name="gender" value="f" />
-	                </label>
-	                
-	            	<div class="clear"></div>
-	            	
-	            	<label>	
-	                	<span>Algo sobre ti</span>
-	                	<textarea name="description" class="wide"></textarea>
-	                </label>
-	                
-	                <div class="clear"></div>
-	                
-	                <label>	
-	                	<span>Nº Teléfon</span>
-	                	<input type="text" name="phone" class="wide" />
-	                </label>
-	                
-	                <label>	
-	                	<span>Website</span>
-	                	<input type="text" name="web" class="wide" />
-	                </label>
-	                
-	                <div class="clear"></div>
-	                
-	                <label>	
-	                	<span>Direcció</span>
-	                	<input type="text" name="address" class="wide address" />
-	                </label>
-	                
-	                <label>	
-	                	<span>C.P.</span>
-	                	<input type="text" name="zip" class="wide" />
-	                </label>
-					
-					<div class="clear"></div>
-					
-					<label>	
-	                	<span>Localidad</span>
-	                	<input type="text" name="city" class="wide" />
-	                </label>
-	                
-	                <label>	
-	                	<span>Provincia</span>
-	                	<input type="text" name="state" class="wide" />
-	                </label>
-	                
-					<label>	
-	                	<span>Pais</span>
-	                	<select name="county">
-	                		<option value="">Seleccione</option>
-	                	</select>
-	                </label>
-	                
-	                <div class="clear"></div>
+	                <div class="box-title color1">
+	    				<p><a href="#" class="toggler">+</a>Suscripción al Newsletter</p>
+	    			</div>
+	    			
+	    			<div class="hide form-toggle-content">
+	    			
+		    			<label>	 
+		    				<input type="radio" name="suscription" value="1" /> Sí 
+		                </label>
+		                
+		                <label>	 
+		    				<input type="radio" name="suscription" value="0" /> No
+		                </label>
+		                
+		                <div class="clear"></div>
+		                
+		        	</div>
 	
 	            </div>    
 	        </div>
@@ -129,6 +166,5 @@
 	    
 	    <div class="clear"></div>
     
-    </form> 
-    
+    </form>     
  
