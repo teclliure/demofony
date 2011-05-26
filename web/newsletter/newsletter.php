284ab63d349ad1456de5e775de958dfa64e764b4
Juan Pablo Lozano <jplozano6@gmail.com>
2011-05-26 14:37:31 +0200

    * html newsletter

---
+++ web/newsletter/newsletter.php
@@ -0,0 +1,102 @@
+<html>
+	<head>
+		<title></title>
+		<style>
+			body{font-family:"Trebuchet MS", Arial, Verdana; font-size:14px; padding:10px;}
+			a{color:#000;}
+		</style>
+	</head>
+	<body style="background-color:#666;">
+	
+		<table style="background-color:#fff; border-collapse:collapse; border:10px solid #fff;" align="center">
+			<tr>
+				<td style="border-bottom:10px solid #fff;" width="628">
+					<img src="header.jpg" />
+				</td>
+			</tr>
+			<tr>
+				<td style="background-color:#BDC4C7;">
+				
+				<br>
+				
+				<table width="550" align="center">
+					<tr>
+						<td >
+				
+					<table>
+						<tr style="font-size:16px; color:#fff;">
+							<td width="300">INICIATIVAS DE ESTA SEMANA</td>
+							<td width="250" style="background-color:#39649A;" align="center">
+								<a href="#" style="text-decoration:none; color:#fff;"><b>FES LA TEVA PROPOSTA</b></a>
+							</td>
+						</tr>
+					</table>
+					
+					<br>
+					
+					<table style="background-color:#fff; margin-bottom:20px;" width="100%">
+						<? for($i = 0; $i < 4; $i++): ?>
+						<tr>
+							<td style="padding:10px; background-color:<?=$i%2?'#fff':'#ddd'?>;">
+								<table>
+									<tr>
+										<td>
+											<img src="http://www.traderscity.com/board/userpixthumbs25/21402-landscape-painting-oil-canvas-oils-art-wholesale-china-landscapes-stretched-arts-1.jpg" border="0" />
+										</td>
+										<td style="font-size:14px;">
+											<a href="#">Esto es un titulo de esto</a><br>
+											<font style="color:#666; font-size:12px;">
+												por <a href="#" style="color:#BD9C3F;">ALGUIEN</a><br>
+												el 23 de marzo de 2011 en <a href="#" style="color:#BD9C3F;">CATEGORIA</a>					
+											</font>
+										</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+						<? endfor; ?>
+					</table>
+					
+					<table>
+						<tr style="font-size:16px; color:#fff;">
+							<td>NOTICIAS</td>
+							<td></td>
+						</tr>
+					</table>
+					
+					<br>
+					
+					<table style="background-color:#fff; margin-bottom:20px;" width="100%">
+						<? for($i = 0; $i < 4; $i++): ?>
+						<tr>
+							<td style="padding:10px; background-color:<?=$i%2?'#fff':'#ddd'?>;">
+								<table>
+									<tr>
+										<td>
+											<img src="http://www.wallpapertube.net/thumbnails/Wide-Overview-of-Mountain-Landscape_7643.jpg" border="0" />
+										</td>
+										<td style="font-size:14px;">
+											<a href="#">Esto es un titulo de esto</a><br>
+											<font style="color:#666; font-size:12px;">
+												sdgs df fsgis gdfigsoaiudgf iuosadgiufasgdouif gsidgfisd fasdafsdfa sdf asdfasd asdfas dfa sdfs adfas fas
+											</font>	
+										</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+						<? endfor; ?>
+					</table>
+					
+							</td>
+						</tr>
+					</table>
+					
+					<br>
+					
+				</td>
+			</tr>
+		</table>
+	
+	</body>
+</html>
