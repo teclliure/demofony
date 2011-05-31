513d8c49e20a5c659dabe2537c8f9928ed404bd9
Marc Montañés <marc@teclliure.net>
2011-05-31 17:17:16 +0200

    * Moved ask question form.

--- apps/frontend/modules/virtualMeeting/templates/showSuccess.php
+++ apps/frontend/modules/virtualMeeting/templates/showSuccess.php
@@ -22,7 +22,7 @@ $(function() {
                   <h1 class="interviewed" style="top: 25px"><?php echo $interview->getsfGuardUser()->getName() ?></h1>
                   <div class="img profile"><img src="<?php echo $interview->getsfGuardUser()->getProfile()->getImageSrcWithDefault('image','main') ?>" alt="<?php echo $interview->getsfGuardUser() ?> image" /></div>
                   <div class="user">
-                    <h1><?php echo $interview->getTitle() ?>fdgsdfg</h1>
+                    <h1><?php echo $interview->getTitle() ?> <span class="misc misc-<?php if($interview->isOpened()): ?>unlock<?php else: ?>lock<?php endif;?> inline"></span></h1>
                     <div style="height: 50px">
                       <p><?php echo __('Participants') ?>: <strong><?php echo $interview->getNumUsers()?></strong></p>
                       <p><?php echo __('Questions') ?>: <strong><?php echo $interview->getNumQuestions()?></strong></p>
@@ -34,9 +34,19 @@ $(function() {
               <div class="about">
                 <?php echo $interview->getBody() ?>
               </div>
-              <div class="answer-own">
-              	<a href="#" class="button1">Answer your own question</a>
-              </div>
+              
+              <?php if ($sf_user->isAuthenticated() && $interview->isOpened()): ?>
+                <div id="form_question" style="display: none; margin: 10px 0 10px 0;">
+                  <form action="<?php echo url_for ('virtualMeeting/addQuestion?id='.$interview->getId()) ?>" class="form" method="post" >
+                  <?php echo $form ?>
+                  <button type="submit" class="button1"><?php echo __('Send')?></button>
+                  </form>
+                </div>
+                <div class="answer-own" id="ask_question">
+                  <a href="#" onClick="$('#ask_question').fadeToggle('slow'); $('#form_question').fadeToggle('slow'); return false;" class="button1"><?php echo __('Ask your question') ?></a>
+                </div>
+              <?php endif; ?>
+              
           </div>
       </div>
     </div>
@@ -49,17 +59,6 @@ $(function() {
           <li><a href="#answered"><b><?php echo __('Answered') ?></b></a></li>
           <li><a href="#not_answered"><b><?php echo __('Not answered') ?></b></a></li>
         </ul>
-        
-        <?php if ($sf_user->isAuthenticated() && $interview->isOpened()): ?>
-          <div id="form_question" style="margin: 30px">
-            <form action="<?php echo url_for ('virtualMeeting/addQuestion?id='.$interview->getId()) ?>" class="form" method="post" >
-            <?php echo $form ?>
-            <button type="submit" class="button1"><?php echo __('Send')?></button>
-            </form>
-          </div>
-        <?php endif; ?>
-              
-        
         
         <div class="box-content"  id="answered">
           <?php include_partial('virtualMeeting/questionList',array('questions'=>$interview->getAnsweredQuestions()))?>
