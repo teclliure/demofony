29e079fbe83939790292de594a98b3cacbfe5010
Marc Montañés <marc@teclliure.net>
2011-05-31 16:48:21 +0200

    * Some more translations

--- apps/backend/modules/commentAdmin/templates/indexSuccess.php
+++ apps/backend/modules/commentAdmin/templates/indexSuccess.php
@@ -15,17 +15,6 @@
   <div id="sf_admin_content">
           <form action="<?php echo url_for('commentAdmin_collection', array('action' => 'batch')) ?>" method="post" id="sf_admin_content_form">
     
-      <div class="sf_admin_actions_block float">
-      	<a tabindex="0" href="#sf_admin_actions_menu" class="fg-button fg-button-icon-right ui-widget ui-state-default ui-corner-all" id="sf_admin_actions_button">
-      	  <span class="ui-icon ui-icon-triangle-1-s"></span>
-      	  <?php echo __('Actions') ?>
-      	</a>
-      	<div id="sf_admin_actions_menu" class="ui-helper-hidden fg-menu fg-menu-has-icons">
-      		<ul class="sf_admin_actions" id="sf_admin_actions_menu_list">
-      			<?php include_partial('commentAdmin/list_actions', array('helper' => $helper)) ?>
-      		</ul>
-      	</div>
-      </div>
 
       <?php include_partial('commentAdmin/list', array('pager' => $pager, 'sort' => $sort, 'helper' => $helper, 'hasFilters' => $hasFilters, 'configuration' => $configuration)) ?>
 
