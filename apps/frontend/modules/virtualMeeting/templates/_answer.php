d76e75d2ca9f0005533a646171d1d4a1d9875a9d
Juan Pablo Lozano <jplozano6@gmail.com>
2011-06-08 15:43:47 +0200

    added button toggler

--- apps/frontend/modules/virtualMeeting/templates/_answer.php
+++ apps/frontend/modules/virtualMeeting/templates/_answer.php
@@ -1,6 +1,7 @@
 <div class="form_answer" style="margin: 30px">
-  <form action="<?php echo url_for ('virtualMeeting/addAnswer?id='.$question->getId()) ?>" class="form" method="post" >
-  <?php echo $formAnswer ?>
-  <button type="submit" class="button1"><?php echo __('Send')?></button>
+  <a href="#" class="button1 toggler-next">Responder</a>
+  <form action="<?php echo url_for ('virtualMeeting/addAnswer?id='.$question->getId()) ?>" class="form" method="post" style="display:none; margin-top:20px;">
+  	<?php echo $formAnswer ?>
+  	<button type="submit" class="button1"><?php echo __('Send')?></button>
   </form>
 </div>
